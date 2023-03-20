4e7328f1c70098942cc0760ced5e1cbf4d48115a
Sadykovrf <85677603+Sadykovrf@users.noreply.github.com>
2023-03-20 01:42:39 +0000

    Update twoSum.hs
    fixed version

--- Haskell/twoSum.hs
+++ Haskell/twoSum.hs
@@ -1,11 +1,12 @@
-ntwoSums:: [Int] -> Int -> Int -> Bool-> Int-> [(Int, Int)]
-ntwoSums [] _ _ _ _ = []
-ntwoSums [_] _ _ _ _ = []
-ntwoSums (x:y:xs) i j f target
-    | target == x + y = [(i, j)]
-    | f==True = ntwoSums (x:xs) (i) (j+1) True target
-    | otherwise = ntwoSums (x:xs) (i) (j+1) True target ++ ntwoSums (y:xs) (i+1) (j+1) False target
-twoSums::[Int] -> Int -> [(Int, Int)]
-twoSums (x:y:xs) target = ntwoSums (x:y:xs) (0::Int) (1::Int) False target
-
-main = putStrLn $ show $ head $ twoSums [2,7,11,15] 9
+twoSums :: [Int] -> Int -> [(Int, Int)]
+twoSums [] _ = []
+twoSums nums target
+  | target > maximum nums = []
+  | otherwise = calc 0 nums []
+  where
+    calc _ [] res = res
+    calc i (x:xs) res =
+      let indices = filter (\j -> nums !! j == target - x) [i+1..length nums-1]
+      in calc (i+1) xs ([(i, j) | j <- indices] ++ res)
+ 
+main = print $ twoSums [2, 7, 11, 15, 2, 7] 9
